4a7f668e3c0aa33fd7e5778776c84dc905ad8627
EvgeniyKononov <kononovea@yandex.ru>
2023-03-03 00:43:13 +0300

    2 Реализовал хранение в БД, добавил класс Booking и логику работы с ним.

--- src/main/java/ru/practicum/shareit/config/DbConfig.java
+++ src/main/java/ru/practicum/shareit/config/DbConfig.java
@@ -24,7 +24,8 @@ import javax.sql.DataSource;
         basePackages = {"ru.practicum.shareit"}
 )
 public class DbConfig {
-    private static final String URL = "jdbc:postgresql://localhost:5432/shareit";
+    //private static final String URL = "jdbc:postgresql://localhost:5432/shareit";
+    private static final String URL = "jdbc:h2:mem:shareit";
     private static final String USERNAME = "test";
     private static final String PASSWORD = "test";
 
@@ -33,7 +34,8 @@ public class DbConfig {
     public DataSource dataSource() {
         return DataSourceBuilder.create()
                 .type(HikariDataSource.class)
-                .driverClassName("org.postgresql.Driver")
+                //.driverClassName("org.postgresql.Driver")
+                .driverClassName("org.h2.Driver")
                 .username(USERNAME)
                 .password(PASSWORD)
                 .url(URL)
