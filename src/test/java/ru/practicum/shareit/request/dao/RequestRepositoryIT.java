558f44b51732ba60913523bfc7e980f76096bd6e
EvgeniyKononov <kononovea@yandex.ru>
2023-03-12 20:42:09 +0300

    2 Реализовал класс Request и работу с ним. Делаю тесты.

--- src/test/java/ru/practicum/shareit/request/dao/RequestRepositoryIT.java
+++ src/test/java/ru/practicum/shareit/request/dao/RequestRepositoryIT.java
@@ -4,13 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @DataJpaTest
 class RequestRepositoryIT {
 
     @Autowired
     private RequestRepository requestRepository;
+
 
     @Test
     void findAllByRequestor_IdOrderByCreationDateTimeDesc() {
