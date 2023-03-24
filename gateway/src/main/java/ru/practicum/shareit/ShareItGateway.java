88271c06d63576a586c075cf28536711bc13014b
EvgeniyKononov <kononovea@yandex.ru>
2023-03-24 10:24:52 +0300

    3 Добавил модуль Gateway

--- gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
+++ gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShareItGateway {
 
     public static void main(String[] args) {
-        System.setProperty("shareit-server.url", "http://localhost:9090");
         SpringApplication.run(ShareItGateway.class, args);
     }
 }
